cc430ca2549289c881eea139545da060fce03183
itsmemohit <mohitdangwal016.md@gmail.com>
2022-09-01 16:11:21 +0530

    fixed bugs and added login form

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,6 +9,16 @@ import Modal from '@mui/material/Modal';
 import {
   FormGroup, Form, Input, Label
 } from 'reactstrap';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const style = {
   position: 'absolute',
@@ -22,12 +32,33 @@ const style = {
   p: 4,
 };
 
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 export default function Topbar() {
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
   return (
     <div className="topbar">
       <div className="topbarWrapper">
@@ -45,13 +76,13 @@ export default function Topbar() {
               aria-labelledby="modal-modal-title"
               aria-describedby="modal-modal-description"
             >
-              <Box sx={style}>
-                {/* <Typography id="modal-modal-title" variant="h6" component="h2">
+              {/* <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
                   Text in a modal
                 </Typography>
                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                   Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography> */}
+                </Typography>
                 <Form>
                   <FormGroup>
                     <Label htmlFor="username">Username</Label>
@@ -72,6 +103,75 @@ export default function Topbar() {
                   </FormGroup>
                   <Button type="submit" value="submit" color="primary">Submit</Button>
                 </Form>
+              </Box> */}
+              <Box sx={style}>
+                <ThemeProvider theme={theme}>
+                  <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                      sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                      </Avatar>
+                      <Typography component="h1" variant="h5">
+                        Sign in
+                      </Typography>
+                      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                          autoFocus
+                        />
+                        <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox value="remember" color="primary" />}
+                          label="Remember me"
+                        />
+                        <Button
+                          type="submit"
+                          fullWidth
+                          variant="contained"
+                          sx={{ mt: 3, mb: 2 }}
+                        >
+                          Sign In
+                        </Button>
+                        <Grid container>
+                          <Grid item xs>
+                            <Link href="#" variant="body2">
+                              Forgot password?
+                            </Link>
+                          </Grid>
+                          <Grid item>
+                            <Link href="#" variant="body2">
+                              {"Don't have an account? Sign Up"}
+                            </Link>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 8, mb: 4 }} />
+                  </Container>
+                </ThemeProvider>
               </Box>
             </Modal>
           </div>
